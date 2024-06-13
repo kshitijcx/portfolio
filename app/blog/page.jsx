2a42cfd7d04fa50ab49ad123e9db259bf42288df
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-06-13 15:46:54 +0530

    blog page design complete

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,7 +1,18 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import LatestBlogsSingleItems from "../_components/LatestBlogsSingleItems";
 
 const Blog = () => {
   return (
-        
-  )
-}
-export default Blog
+    <ScrollArea className="max-w-3xl mx-auto h-[500px] rounded-md border p-4">
+      <div className="flex flex-col gap-2 mx-auto max-w-xl">
+        <LatestBlogsSingleItems />
+        <LatestBlogsSingleItems />
+        <LatestBlogsSingleItems />
+        <LatestBlogsSingleItems />
+        <LatestBlogsSingleItems />
+        <LatestBlogsSingleItems />
+      </div>
+    </ScrollArea>
+  );
+};
+export default Blog;
