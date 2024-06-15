22ef58dace89889941a66e9781e724a2c0ae448e
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-06-16 00:39:01 +0530

    post filtering logic

--- app/blog/_components/BlogCard.jsx
+++ app/blog/_components/BlogCard.jsx
@@ -11,7 +11,7 @@ import { format } from "date-fns";
 import { Badge } from "@/components/ui/badge";
 import { useState } from "react";
 
-const BlogCard = ({ item }) => {
+const BlogCard = ({ item,filterFxn }) => {
   return (
     <Card className="flex items-center px-6 py-1"> 
       <div className="w-32 max-md:hidden">
@@ -32,7 +32,7 @@ const BlogCard = ({ item }) => {
           </CardDescription>
         </CardHeader>
         <CardFooter className="px-4 py-2 text-xs space-x-1">
-          {item.categories.map((cat,index)=>(<Badge className="text-xs" key={index}>{cat.title}</Badge>))}
+          {item.categories.map((cat,index)=>(<Badge className="text-xs" key={index} onClick={()=>filterFxn(cat.title)}>{cat.title}</Badge>))}
         </CardFooter>
       </div>
     </Card>
--- app/blog/_components/FilterCompo.jsx
+++ app/blog/_components/FilterCompo.jsx
@@ -1,14 +1,46 @@
-"use client"
+"use client";
 import { useState } from "react";
 import BlogCard from "./BlogCard";
+import { X } from "lucide-react";
 
-const FilterCompo = ({data}) => {
-  const [filerItems, setFilterItems] = useState('');
+const FilterCompo = ({ data }) => {
+  const [filterItems, setFilterItems] = useState(data);
+  const [filterTag, setFilterTag] = useState("");
+
+  function filterFxn(cat) {
+    setFilterTag(cat);
+    const newItems = data.filter((item) => {
+      let v = false;
+      item.categories.map((c) => {
+        if (c.title === cat) {
+          v = true;
+        }
+      });
+      return v;
+    });
+    setFilterItems(newItems);
+  }
+
   return (
-    <div className="flex flex-col h-full gap-2 mx-auto max-w-xl">
-      {data.map((item) => (
-        <BlogCard key={item._id} item={item} setFilterItems={setFilterItems} />
-      ))}
+    <div>
+      {filterTag && (
+        <div className="text-sm mb-3 text-neutral-500 flex justify-center items-center gap-2">
+          <p>Tag: </p>
+          {filterTag}
+          <X size={14}
+            onClick={() => {
+              setFilterItems(data);
+              setFilterTag("");
+            }}
+            className="hover:-translate-y-[1.2px] hover: cursor-pointer hover:text-white"
+          />{" "}
+        </div>
+      )}
+      <div className="flex flex-col h-full gap-2 mx-auto max-w-xl">
+        {filterItems.map((item) => (
+          <BlogCard key={item._id} item={item} filterFxn={filterFxn} />
+        ))}
+      </div>
     </div>
   );
 };
