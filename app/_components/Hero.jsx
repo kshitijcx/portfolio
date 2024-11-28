f442c531c74355c5d6a9c0a9017e6bd1351d94ae
Kshitij Chauhan <99241138+kshitijcx@users.noreply.github.com>
2024-11-28 20:02:40 +0530

    update links

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -31,13 +31,13 @@ const Hero = () => {
       <CardContent className="flex gap-2 justify-end max-md:justify-start max-md:px-6 max-md:pb-3 max-md:pt-2">
         <Link
           className={`${buttonVariants({ variant: "secondary" })} max-md:h-8 max-md:w-8 max-md:p-1`}
-          href="https://github.com/kshi7ijcx"
+          href="https://github.com/kshitijcx"
         >
           <GitHubLogoIcon className="max-md:w-4 max-md:h-4 h-5 w-5" />
         </Link>
         <Link
           className={`${buttonVariants({ variant: "secondary" })} max-md:h-8 max-md:w-8 max-md:p-1`}
-          href="https://www.linkedin.com/in/kshitij-chauhan-161444223/"
+          href="https://www.linkedin.com/in/kshitijcx/"
         >
           <LinkedInLogoIcon className="max-md:w-4 max-md:h-4 h-5 w-5" />
         </Link>
